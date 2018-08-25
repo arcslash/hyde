316c0ee313c0ca705263b80d743e7eafa3884760
Ishara Upamal <isharaux@gmail.com>
2018-08-25 09:48:55 +0530

    Modifications on training UI, To show points on mouse clicks

--- static/custom.js
+++ static/custom.js
@@ -6,11 +6,10 @@ var style = window.getComputedStyle(document.documentElement);
 var modal = document.getElementById('myModal');
 var canvas= document.getElementById("pointsCanvas"),ctx = canvas.getContext("2d");
 
+//mouse move positions
+var xpos;
+var ypos;
 
-ctx.fillStyle = "red";
-ctx.moveTo(0, 0);
-ctx.lineTo(200, 100);
-ctx.stroke();
 function measure(){
   $.get('http://localhost:5000/measure',function(data){
     alert( data );
@@ -38,13 +37,50 @@ function initApp(){
   camera();
   var img = document.getElementById('canvas_image');
 //or however you get a handle to the IMG
-  $("#pointsCanvas").width = img.clientWidth;
-  $("#pointsCanvas").height = img.clientHeight;
+  ctx.canvas.width = img.clientWidth;
+  ctx.canvas.height = img.clientHeight;
+  ctx.fillStyle = "white";
+  ctx.moveTo(0, 0);
+  ctx.lineTo(200, 100);
+  ctx.stroke();
 
 
 }
-window.onload = initApp;
+function getMouseMoves(mouseEvent){
+  var obj = document.getElementById("pointsCanvas");
+  var obj_left = 0;
+  var obj_top = 0;
 
+  while (obj.offsetParent){
+    obj_left += obj.offsetLeft;
+    obj_top += obj.offsetTop;
+    obj = obj.offsetParent;
+  }
+  if (mouseEvent){
+    //FireFox
+    xpos = mouseEvent.pageX;
+    ypos = mouseEvent.pageY;
+  }else{
+    //IE
+    xpos = window.event.x + document.body.scrollLeft - 2;
+    ypos = window.event.y + document.body.scrollTop - 2;
+  }
+  xpos -= obj_left;
+  ypos -= obj_top;
+  document.getElementById("cordinates").innerHTML = xpos + ", " + ypos;
+}
+
+function getMouseClick(){
+    var pointSize = 3; // Change according to the size of the point.
+    ctx.fillStyle = "#ff2626"; // Red color
+
+    ctx.beginPath(); //Start path
+    ctx.arc(xpos, ypos, pointSize, 0, Math.PI * 2, true); // Draw a point using the arc function of the canvas with a point structure.
+    ctx.fill(); // Close the path and fill.
+}
+window.onload = initApp;
+document.getElementById("pointsCanvas").onmousemove = getMouseMoves;
+document.getElementById("pointsCanvas").onmouseup = getMouseClick;
 
 //
 // // Get the modal
